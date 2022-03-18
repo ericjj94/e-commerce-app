ce44d1590556660e7d018affd89f94c5aadd8a75
Eric <ericjj94@gmail.com>
2022-03-18 22:12:29 +0530

    input field width adjusted

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
           E-commerce app
         </a>
         <form className="d-flex">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <input className="form-control me-2 search-input" type="search" placeholder="Search" aria-label="Search" />
           <button className="btn btn-outline-success" type="submit">
             Search
           </button>
